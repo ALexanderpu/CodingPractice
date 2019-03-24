1e899f3efb92a2dc87affd17bfe54946d74bd674
Alexander <pubo.alexander@gmail.com>
2019-03-24 16:00:28 -0600

    Create OversizedPancake.cpp

---
+++ OversizedPancake.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define filein freopen("A-large.in", "r", stdin);
+#define fileout freopen("a.out", "w", stdout);
+#define ll long long
+#define rep(i , j , n) for(ll i = j ; i <= n ; i++)
+
+void solve(tc){
+    string s;
+    int k;
+		cin >> s >> k;
+		int n = s.length(), ans = 0;
+		for (int i = 0; i + k <= n; i++) { 
+      if (s[i] == '+') continue;
+      // flip '-' to '+' for continuous subarray
+      for (int j = i; j < i + k; j++)
+        s[j] = (s[j] == '+') ? '-' : '+';
+      ans++;
+		}
+		bool flag = true;
+		for (int i = 0; flag && i < n; i++)
+			 flag = (s[i] == '+');
+      
+		cout << "Case #" << tc << ": ";
+		if (flag)
+			cout << ans << endl;
+		else
+			cout << "IMPOSSIBLE"<< endl;
+}
+
+int main(){
+    filein;
+    fileout;
+    int kase;
+    cin >> kase;
+    rep(tc, 1, kase){
+        solve(tc);
+    }
+    return 0;
+}
